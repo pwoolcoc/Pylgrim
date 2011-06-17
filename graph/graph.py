392996f1e292c6bd9d169ed436b932fee1242ef0
Paul Woolcock <pwoolcoc@umflint.edu>
2011-06-17 01:26:39 -0400

    add new class graph.Element, that is the base class for Vertex and Edge. I want to eventually let the user define arbitrary properties on Vertex and Edge objects, and the deepcopy that happens in Element.__init__ gets an initial copy of self.__dict__ to be used in Element.__setattr__ later

--- graph/graph.py
+++ graph/graph.py
@@ -1,8 +1,15 @@
+import copy
 import uuid
 
-class Vertex(object):
+class Element(object):
     def __init__(self, *args, **kwds):
+        self.__initial_properties__ = copy.deepcopy(self.__dict__)
+
+class Vertex(Element):
+    def __init__(self, obj=None, *args, **kwds):
         self.id = uuid.uuid4().hex
+        self.obj = obj
+        super(Vertex, self).__init__(*args, **kwds)
 
     def edgeto(self, to, weight=None):
         return Edge(from_=self, to=to, weight=weight)
@@ -16,9 +23,10 @@ class Vertex(object):
     def __lshift__(self, from_):
         return self.edgefrom(from_)
 
-class Edge(object):
+class Edge(Element):
     def __init__(self, from_, to, weight=None, *args, **kwds):
         self.from_ = from_
         self.to = to
         self.weight = weight
+        super(Edge, self).__init__(*args, **kwds)
 
