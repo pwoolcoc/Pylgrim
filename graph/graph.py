0c633c39d3c99e6322bf5b8c545683dcbc9b0bc2
Paul Woolcock <pwoolcoc@umflint.edu>
2011-06-19 00:12:36 -0400

    add tests and code for in_(), inE(), out(), outE(), both(), and bothE() to Vertex

--- graph/graph.py
+++ graph/graph.py
@@ -9,6 +9,10 @@ class Vertex(Element):
     def __init__(self, obj=None, *args, **kwds):
         self.uuid = uuid.uuid4().hex
         self.obj = obj
+        self._out = []
+        self._outE = []
+        self._in_ = []
+        self._inE = []
         super(Vertex, self).__init__(*args, **kwds)
 
     def edgeto(self, to, weight=None):
@@ -24,7 +28,16 @@ class Vertex(Element):
 
         >>> e = v.edgefrom(u)
         """
-        return Edge(from_=self, to=to, weight=weight)
+        e = Edge(from_=self, to=to, weight=weight)
+
+        # Bookkeeping
+        self._out.append(to)
+        self._outE.append(e)
+
+        to._in_.append(self)
+        to._inE.append(e)
+
+        return e
 
     def edgefrom(self, from_, weight=None):
         """
@@ -32,13 +45,35 @@ class Vertex(Element):
 
         Defines the reverse relationship that `edgeto` does
         """
-        return Edge(from_=from_, to=self, weight=weight)
+        e = Edge(from_=from_, to=self, weight=weight)
+
+        # Bookkeeping
+        self._in_.append(from_)
+        self._inE.append(e)
+
+        from_._out.append(self)
+        from_._outE.append(e)
+
+        return e
 
     def __rshift__(self, to):
         return self.edgeto(to)
 
     def __lshift__(self, from_):
         return self.edgefrom(from_)
+
+    def out(self):
+        return self._out
+    def outE(self):
+        return self._outE
+    def in_(self):
+        return self._in_
+    def inE(self):
+        return self._inE
+    def both(self):
+        return self._out + self._in_
+    def bothE(self):
+        return self._outE + self._inE
 
 class Edge(Element):
     def __init__(self, from_, to, weight=None, *args, **kwds):
